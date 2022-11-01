38e6f2ac0ff470036d9fb595f8028d0dd3aa01cd
ASUS <ASUS@DESKTOP-TIUS30F>
2022-11-01 10:11:29 +0100

    Ajoutez des fichiers projet.

---
+++ EXERCICE1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICE1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n = 5, a = 0;
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    a++;
+
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} est un nombre premier, n");
+
+            } else { 
+                Console.WriteLine("n'est pas un nombre premier");
+            }
+            Console.ReadLine();
+        }
+    }
+}
